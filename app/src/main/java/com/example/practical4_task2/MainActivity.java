c2816502b796e77e62536107bb9973090b14f28d
ANN <nufailnurhan@gmail.com>
2022-12-14 15:56:08 +0800

    first commit

--- app/src/main/java/com/example/practical4_task2/MainActivity.java
+++ app/src/main/java/com/example/practical4_task2/MainActivity.java
@@ -16,24 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button BtnSubmit = findViewById(R.id.BtnSubmit);
-        BtnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CheckBox CBRed = findViewById(R.id.CBRed);
-                CheckBox CBGreen = findViewById(R.id.CBGreen);
-                CheckBox CBBlue = findViewById(R.id.CBBlue);
-
-                ConstraintLayout CSLayout = findViewById(R.id.CLayoutCS);
-
-                if(CBRed.isChecked())
-                    CSLayout.setBackgroundColor(Color.RED);
-                else if(CBGreen.isChecked())
-                    CSLayout.setBackgroundColor(Color.GREEN);
-                else
-                    CSLayout.setBackgroundColor(Color.BLUE);
-            }
-        });
     }
 
 }
