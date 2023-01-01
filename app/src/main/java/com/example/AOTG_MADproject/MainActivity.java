1dd6e50f352d9952a0f6b185357a9c4da2310019
Mariam Azlina <mriamzlin02@gmail.com>
2023-01-01 12:24:45 +0800

    first commit

--- app/src/main/java/com/example/AOTG_MADproject/MainActivity.java
+++ app/src/main/java/com/example/AOTG_MADproject/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.practical4_task2.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
